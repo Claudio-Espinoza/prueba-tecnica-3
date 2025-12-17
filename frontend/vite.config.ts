fa51bbc7fd7266d01c34885eea63858a763961f4
Claudio Espinoza <claudioyugen@gmail.com>
2025-12-17 20:15:29 -0300

    feat(frontend): agrega rutas personalizadas

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@features': resolve(__dirname, './src/features'),
+      '@libraries': resolve(__dirname, './src/libraries'),
+      '@platforms': resolve(__dirname, './src/platforms'),
+    },
+  },
+
 })
