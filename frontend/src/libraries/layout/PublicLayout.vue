bae637e70a42357028a67ba2bb976d9383d4fc7e
Claudio Espinoza <claudioyugen@gmail.com>
2025-12-17 20:53:59 -0300

    feat(frontend): agrega componente layout

---
+++ frontend/src/libraries/layout/PublicLayout.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+</script>
+
+<template>
+   <header
+      class="w-full h-18 px-xl py-3 gap-0 bg-neutral-50 border border-neutral-400 rounded-full flex flex-row items-center justify-between"
+   >
+      <div class="h-full w-fit flex flex-row justify-center items-center gap-4">
+         <img src="/vite.svg" class="w-fit h-full" alt="" />
+         <div class="size-full">
+            <h1>Prueba Técnica 3</h1>
+            <p>Estado: <a class="text-feedback-success-300">Conectado</a></p>
+         </div>
+      </div>
+      <nav>
+         <ul>
+            <li><router-link to="/">Home</router-link></li>
+         </ul>
+      </nav>
+   </header>
+
+   <main class="size-full flex flex-row gap-l items-center justify-center">
+      <router-view />
+   </main>
+</template>
